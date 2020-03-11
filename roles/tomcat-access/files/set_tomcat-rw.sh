0b672784aa9c9d1db14ab032c13d9e61ef8e4053
woocash <woocashboczkaja@gmail.com>
2020-03-11 12:48:01 +0100

    v1.0 - works with defined access level RO, RW, operator

---
+++ roles/tomcat-access/files/set_tomcat-rw.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TOMCAT_HOME=$(ps -ef | egrep -i java | grep -P -o "catalina.base=.*? " | cut -d '=' -f2)
+# Wyszukaj katalog domowy tomcata i przypisz uprawnienia do zapisu i odczytu - dla katalogów rwx, dla plików rw
+find $TOMCAT_HOME -follow -type d -exec setfacl -m g:tomcat-rw:rwx '{}' \;
+find $TOMCAT_HOME -follow -type f -exec setfacl -m g:tomcat-rw:rw '{}' \;
