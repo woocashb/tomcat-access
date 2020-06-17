ce97995c5e5e89c81e816697c7d5ebc59d3ad70e
woocash <woocashboczkaja@gmail.com>
2020-06-17 11:43:20 +0200

    Added TOMCAT_HOME value validation in permission-setup bash scripts

--- roles/tomcat-access/files/set_tomcat-ro.sh
+++ roles/tomcat-access/files/set_tomcat-ro.sh
@@ -2,5 +2,9 @@
 
 TOMCAT_HOME=$(ps -ef | egrep -i java | grep -P -o "catalina.base=.*? " | cut -d '=' -f2)
 # Wyszukaj katalog domowy tomcata i przypisz uprawnienia do zapisu i odczytu - dla katalogów rwx, dla plików rw
+if [[ -z $TOMCAT_HOME ]];then
+   echo "Nie udało się ustalić katalogu domowego Tomcata - przerywam działanie, czy usługa jest włączona?"
+   exit 1
+fi
 find $TOMCAT_HOME -follow -type d -exec setfacl -m g:tomcat-ro:rx '{}' \;
 find $TOMCAT_HOME -follow -type f -exec setfacl -m g:tomcat-ro:r '{}' \;
